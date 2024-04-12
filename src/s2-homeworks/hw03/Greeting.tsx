2379856f4776b03978238645d1e47b6b4cde081e
apastan <apastan.dev@gmail.com>
2024-04-12 23:59:30 +0300

    02-03: Fix error in Greetings component

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -26,7 +26,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.input + " " + (s.error ? s.errorInput : "") // need to fix with (?:)
+    const inputClass = s.input + " " + (error ? s.errorInput : "") // need to fix with (?:)
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
