603ba882c9f903f7a66a068b6438cef0e422272a
apastan <apastan.dev@gmail.com>
2024-06-18 20:17:45 +0300

    04-03 feat: slider MUI styles

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,9 +5,20 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: 200,
+                '& .MuiSlider-thumb': {
+                    border: "1px solid #00CC22",
+                    color: '#fff', // Customize thumb (picker) color
+                },
+                '& .MuiSlider-rail': {
+                    color: '#8B8B8B', // Customize rail color
+                },
+                '& .MuiSlider-track': {
+                    color: '#00CC22', // Customize track color
+                },
             }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            {...props}
+            // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
 }
