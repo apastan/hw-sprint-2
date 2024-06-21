2d9c21c026926ad592284591fa6cea48eaa2fec5
apastan <apastan.dev@gmail.com>
2024-06-21 12:53:59 +0300

    04-04 feat: fix logic in themeReducer

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,22 @@
-const initState = {
+export type Theme = 1 | 2 | 3
+
+type ThemeState = {
+    themeId: Theme
+}
+
+const initState: ThemeState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+type ThemeActions = ReturnType<typeof changeThemeId>
 
+export const themeReducer = (state = initState, action: ThemeActions): ThemeState => { // fix any
+    switch (action.type) {
+        case "SET_THEME_ID":
+            return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: Theme) => ({type: 'SET_THEME_ID', id}) as const
